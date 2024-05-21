b598067ce19538217704e434d40eb5e5c18bf193
DEVAVISWA <devaviswa15@gmail.com>
2024-05-21 19:46:01 +0530

    initial page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import InstructionPage from "./components/InstructionPage/InstructionPage";
+import Pokemon from "./components/PokemonPage/Pokemon";
+import LikedPokemon from "./components/LikedPokemon/LikedPokemon";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<InstructionPage />} />
+        <Route path="/pokemon" element={<Pokemon />} />
+        <Route path="/pokemon_liked" element={<LikedPokemon />} />
+      </Routes>
+    </Router>
+  );
 }
---
+++ src/components/InstructionPage/InstructionPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function InstructionPage() {
+  return (
+    <div>
+      <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="" />
+      <div>
+        
+      </div>
+    </div>
+  )
+}
